af884c08e0ab62cbfc5acc635a0b6177dd6c956d
Ramiro Lopez <lopezdiaz.ramiro@gmail.com>
2016-10-08 09:23:05 -0700

    Small UI Tweaks

--- models/burger.js
+++ models/burger.js
@@ -4,23 +4,23 @@ var orm = require('../config/orm.js');
 
 var burgers = {
 	all: function (cb) {
-		orm.all('burgers', function (res) {
+		orm.all('Burgers', function (res) {
 			cb(res);
 		});
 	},
 	//COLS AND VALS ARE ARRAYS	
 	create: function(cols, vals, cb) {
-		orm.create('burgers', cols, vals, function (res){
+		orm.create('Burgers', cols, vals, function (res){
 			cb(res);
 		});
 	},
 	devour: function (objColVals, condition, cb) {
-		orm.devour('burgers', objColVals, condition, function (res) {
+		orm.devour('Burgers', objColVals, condition, function (res) {
 			cb(res);
 		})
 	},
 	clear: function (condition, cb) {
-		orm.clear('burgers', condition, function (res) {
+		orm.clear('Burgers', condition, function (res) {
 			cb(res);
 		});
 	}
